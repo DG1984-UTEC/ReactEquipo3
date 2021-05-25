4794981a65d451c25f3fe9c841349d80169760d6
alvaro.munoz <alvaro.munoz@utec.edu.uy>
2021-05-24 23:35:56 -0300

    se agrega pagina para editar post-it con todo su estilo, ademas se subi inicialmente una lista al localStorage

--- post-it/src/list_post_it/ListPostIt.jsx
+++ post-it/src/list_post_it/ListPostIt.jsx
@@ -1,13 +1,23 @@
 import './list_post_it.css';
+import {Link} from 'react-router-dom'
 /*import { post_it } from './model/post_it';*/
 
+/*{id:4,note:"nota4"},{id:5,note:"nota5"},{id:6,note:"nota6"},{id:7,note:"nota7"}*/
+
+let list_post_it=[{id:1,note:"nota1"}, {id:2,note:"nota2"},{id:3,note:"nota3"}];
+localStorage.setItem("listaNotas", JSON.stringify(list_post_it));
 
 
 
 
 
 function ListPostIt(){
-    let list_post_it=[{id:1,note:"nota1"}, {id:2,note:"nota2"},{id:3,note:"nota3"},{id:4,note:"nota4"},{id:5,note:"nota5"},{id:6,note:"nota6"},{id:7,note:"nota7"}];  
+      
+    console.log("LISTA OBTENIDA DESDE LOCAL",localStorage.getItem("listaNotas"));
+
+    let listaPostIt=JSON.parse(localStorage.getItem("listaNotas"));
+
+    
 
     function editPostIt(postIt){
         console.log(postIt);
@@ -16,11 +26,12 @@ function ListPostIt(){
     return (
         <div className="postItContainer">
             <h1>Notas</h1>
-            <div className="postItWrap">
+            <div to="/editPostIt" className="postItWrap">
                 {
-                    list_post_it.map(element => (
+                    
+                    listaPostIt.map(element => (
                         
-                        <div onClick={()=>editPostIt(element)} className="postIt" key={element.id}>{element.note}</div>
+                        <Link to="/editPostIt" onClick={()=>editPostIt(element)} className="postIt" key={element.id}>{element.note}</Link>
                     ))
                 }           
             </div>
